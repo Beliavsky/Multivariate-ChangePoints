4b24b2324ddc495736043f71ba81f2701fac8f96
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2026-04-13 23:21:17 -0400

    Add files via upload

---
+++ constants.f90
@@ -0,0 +1,9 @@
+module constants_mod
+    use kind_mod
+    implicit none
+    public
+
+    real(dp), parameter :: pi = 3.14159265358979323846_dp
+    real(dp), parameter :: catalan = 0.91596559417721901505_dp
+
+end module constants_mod
